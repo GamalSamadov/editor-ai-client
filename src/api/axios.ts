ed41391e63fc937ddc0dbd4958348dd188b93dd4
Jamoliddin Samadov <gamalsamadov@gmail.com>
2025-03-23 12:53:30 +0300

    FIX: vercel deploy

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,53 +1,54 @@
-import { API_URL } from '@/utils/constants'
-import axios, { CreateAxiosDefaults } from 'axios'
-import { errorCatch, getContentType } from './api.helper'
-import { getAccessToken, removeFromStorage } from '@/services/auth/auth.helper'
-import { authService } from '@/services/auth/auth.service'
+import { API_URL } from "@/utils/constants"
+import axios, { CreateAxiosDefaults } from "axios"
+import { errorCatch, getContentType } from "./api.helper"
+import { getAccessToken, removeFromStorage } from "@/services/auth/auth.helper"
+import { authService } from "@/services/auth/auth.service"
 
 const axiosOptions: CreateAxiosDefaults = {
-	baseURL: API_URL,
-	headers: {
-		...getContentType(),
-	},
+  baseURL: API_URL,
+  headers: {
+    ...getContentType(),
+  },
+  withCredentials: true,
 }
 
 export const axiosClassic = axios.create(axiosOptions)
 
-axiosClassic.interceptors.request.use(config => {
-	const accessToken = getAccessToken()
+axiosClassic.interceptors.request.use((config) => {
+  const accessToken = getAccessToken()
 
-	if (config.headers && accessToken) {
-		config.headers.Authorization = `Bearer ${accessToken}`
-	}
+  if (config.headers && accessToken) {
+    config.headers.Authorization = `Bearer ${accessToken}`
+  }
 
-	return config
+  return config
 })
 
 axiosClassic.interceptors.response.use(
-	config => config,
-	async error => {
-		const originalRequest = error.config
+  (config) => config,
+  async (error) => {
+    const originalRequest = error.config
 
-		if (
-			(error?.response?.status === 401 ||
-				errorCatch(error) === 'jwt expired' ||
-				errorCatch(error) === 'jwt must be provided') &&
-			error.config &&
-			!error.config._isRetry
-		) {
-			originalRequest._isRetry = true
-			try {
-				await authService.getNewTokens()
-				return axiosClassic.request(originalRequest)
-			} catch (error) {
-				if (
-					errorCatch(error) === 'jwt expired' ||
-					errorCatch(error) === 'Refresh token not passed'
-				)
-					removeFromStorage()
-			}
-		}
+    if (
+      (error?.response?.status === 401 ||
+        errorCatch(error) === "jwt expired" ||
+        errorCatch(error) === "jwt must be provided") &&
+      error.config &&
+      !error.config._isRetry
+    ) {
+      originalRequest._isRetry = true
+      try {
+        await authService.getNewTokens()
+        return axiosClassic.request(originalRequest)
+      } catch (error) {
+        if (
+          errorCatch(error) === "jwt expired" ||
+          errorCatch(error) === "Refresh token not passed"
+        )
+          removeFromStorage()
+      }
+    }
 
-		throw error
-	}
+    throw error
+  }
 )
