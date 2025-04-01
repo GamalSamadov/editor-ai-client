db43ec293ad2aeb26adfddc39d0167e1fafba124
Jamoliddin Samadov <gamalsamadov@gmail.com>
2025-04-01 11:06:41 +0300

    FIX: initial theme issue

--- src/providers/providers.tsx
+++ src/providers/providers.tsx
@@ -1,26 +1,26 @@
-'use client'
+"use client"
 
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { PropsWithChildren, useState } from 'react'
-import { ThemeProvider } from './theme'
-import { Toaster } from 'sonner'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { PropsWithChildren, useState } from "react"
+import { ThemeProvider } from "./theme"
+import { Toaster } from "sonner"
 
 const Providers = ({ children }: PropsWithChildren) => {
-	const [client] = useState(new QueryClient())
+  const [client] = useState(new QueryClient())
 
-	return (
-		<QueryClientProvider client={client}>
-			<ThemeProvider
-				attribute='class'
-				defaultTheme='system'
-				enableSystem
-				disableTransitionOnChange
-			>
-				<Toaster />
-				{children}
-			</ThemeProvider>
-		</QueryClientProvider>
-	)
+  return (
+    <QueryClientProvider client={client}>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="dark"
+        enableSystem
+        disableTransitionOnChange
+      >
+        <Toaster />
+        {children}
+      </ThemeProvider>
+    </QueryClientProvider>
+  )
 }
 
 export default Providers
